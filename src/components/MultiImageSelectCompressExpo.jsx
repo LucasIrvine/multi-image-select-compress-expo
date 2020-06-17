e11b411db69f98fb44ddbb787e290e04619a080c
lucasirvine <lucasirvine@gmail.com>
2020-06-17 14:38:17 -0500

    set up testing with fake test, tweaking linting, added react and RN

---
+++ src/components/MultiImageSelectCompressExpo.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+const styles = StyleSheet.create({
+	container: {
+		width: "100%",
+		height: "100%",
+		position: "absolute",
+		top: 0,
+		left: 0,
+		zIndex: 300,
+		backgroundColor: "#fefefe",
+	},
+});
+
+export default () => {
+	const renderImages = () => {};
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<Text>Hello bud</Text>
+			<View>{renderImages()}</View>
+		</SafeAreaView>
+	);
+};
