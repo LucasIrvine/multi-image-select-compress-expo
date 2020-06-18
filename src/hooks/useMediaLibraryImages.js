a6920114f1c9fdeffeaa5837fdfd8bac49368826
lucasirvine <lucasirvine@gmail.com>
2020-06-18 18:07:22 -0500

    Revert "parameterizing all the options requirements"
    This reverts commit b72213742cc4029af723cc70e46ed89ec4f03931.

---
+++ src/hooks/useMediaLibraryImages.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import * as MediaLibrary from "expo-media-library";
+
+const useMediaLibraryImages = () => {
+	const [initialFetch, setInitialFetch] = useState(false);
+	const [images, setImages] = useState([]);
+	const [pagingCursor, setPagingCursor] = useState(false);
+	const [hasNextPage, setHasNextPage] = useState(true);
+	const [isImagesLoading, setIsImagesLoading] = useState(false);
+	const [isImagesError, setIsImagesError] = useState(false);
+
+	const fetchMedia = async () => {
+		// set initial fetch
+		if (!initialFetch) {
+			setInitialFetch(true);
+		}
+		// reset flags
+		setIsImagesError(false);
+		setIsImagesLoading(true);
+		// try to fetch media from expo-media-library
+		try {
+			// initial params
+			const params = {
+				first: 50,
+			};
+			// if current count
+			if (pagingCursor) {
+				params.after = pagingCursor;
+			}
+			// if no media pages left just return
+			if (!hasNextPage) return;
+			// get next media chunk
+			const newMedia = await MediaLibrary.getAssetsAsync(params);
+			// just return if end cursor
+			if (pagingCursor === newMedia.endCursor) return;
+			// append new images, update cursor and hasNextPage flag
+			setImages([...images, ...newMedia.assets]);
+			setPagingCursor(newMedia.endCursor);
+			setHasNextPage(newMedia.hasNextPage);
+		} catch (error) {
+			setIsImagesError(true);
+		}
+
+		setIsImagesLoading(false);
+	};
+
+	if (!initialFetch) {
+		fetchMedia();
+	}
+
+	return [
+		{
+			images,
+			pagingCursor,
+			hasNextPage,
+			isImagesLoading,
+			isImagesError,
+			fetchMedia,
+		},
+		[],
+	];
+};
+
+export default useMediaLibraryImages;
