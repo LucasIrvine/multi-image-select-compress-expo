95586734986a3caddc116d202cac57cae2db4a16
lucasirvine <lucasirvine@gmail.com>
2020-06-18 18:03:29 -0500

    Revert "fixed origin and updated readme"
    This reverts commit 8c11de19c5afda86d230c0690c0ee56ad8df6557.

--- src/constants/Colors.js
+++
@@ -1,8 +0,0 @@
-export default {
-	cancelButton: "rgb(0,122,255)",
-	confirmButton: "rgb(0,122,255)",
-	headingText: "#000000",
-	selectedCheck: "rgb(76,217,100)",
-	normalText: "#000000",
-	lightText: "#999999",
-};
